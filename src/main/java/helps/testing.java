09e135db19e001bda42a2b694fa8cc858fad0fa1
I1538 <I1538@ISILLT10372.indium.com>
2019-05-21 18:29:42 +0530

    updated testing.java file

--- src/main/java/helps/testing.java
+++ src/main/java/helps/testing.java
@@ -1,9 +1,13 @@
 package helps;
 
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class testing 
 {
 	public static void main(String[] args) 
 	{
+		RemoteWebDriver driver;
 		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 	}
 }
